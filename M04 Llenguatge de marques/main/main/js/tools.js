12122ec08526f7aa9332af1d6e90a9d09c5a1227
Victor Valero <vvalerolopez.eb@iesesteveterradas.cat>
2024-01-12 19:21:20 +0100

    cambio de foto

--- M04 Llenguatge de marques/main/main/js/tools.js
+++
@@ -1,9 +0,0 @@
-export function getShaderSource (elementId) {
-    var element = document.getElementById(elementId);
-
-    if (element == null) {
-        throw new Error("Elemento shader não encontrado.");
-    }
-
-    return element.text;
-}
