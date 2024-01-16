9bc730cf939d494925f0399cc40b4c9976614a85
Victor Valero <vvalerolopez.eb@iesesteveterradas.cat>
2024-01-16 15:00:45 +0100

    carrusel img, y canvas puesto como background

--- M04 Llenguatge de marques/main/main/main.js
+++ M04 Llenguatge de marques/main/main/main.js
@@ -720,3 +720,31 @@
         document.getElementById('myModal').style.display = 'none';
         document.getElementById('videoModal').pause();
     });
+
+/* CARRUSEL */
+
+const scrollers = document.querySelectorAll(".scroller");
+
+if (!window.matchMedia("(prefers-reduced-motion: none)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    // add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
