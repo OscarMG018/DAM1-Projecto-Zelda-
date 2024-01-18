56876ba51b836bd4b56626af2b9ce69507925da7
Victor Valero <vvalerolopez.eb@iesesteveterradas.cat>
2024-01-18 19:42:17 +0100

    Reestructuración de directorios

--- M04 Llenguatge de marques/main/sidebar/sidebar.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    let menuBtn = document.getElementById('menu');
-    let menuItem = document.getElementById('menuItem');
-    menuBtn.classList.toggle('active')
-    menuItem.classList.toggle('active')
-}
